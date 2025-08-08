6462e11d505ba836908367180efc63c9ed5e8a05
naomi cjidera <naomichidera02@gmail.com>
2025-08-08 02:15:36 -0700

    folder to learn java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        add();
+    }
+    public static int add(){
+        int c = 2+3;
+        System.out.println(c);
+        return c;
+    }
+}
+//javac Main.java --compile code
+//java Main--run the code
